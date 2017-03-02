f706d90c6d0e77b62fd963ac347e746638d5232c
Anastasia <anastasia.diudina@gmail.com>
2017-03-02 08:55:41 +0300

    little style fixes

--- apps/processing/management/commands/batch_settle.py
+++ apps/processing/management/commands/batch_settle.py
@@ -3,9 +3,9 @@ import datetime
 from django.core.management.base import BaseCommand, CommandError
 
 from apis import SchemaAPI, TelegramAPI, \
-                 SmsAPI, SendgridAPI
+    SmsAPI, SendgridAPI
 from apps.processing.models import Transaction, \
-                                   UserAccountsUnion
+    UserAccountsUnion
 from card_issuing_excercise.settings import AUTHORISATION_TRANSACTION_TTL
 from utils import to_start_day
 
@@ -18,21 +18,23 @@ class Command(BaseCommand):
         # and log it as our transactions
         self._transfer_settlements_to_schema()
         # and rollback pld transactions
-        self._rollback_outdated_transactions() 
-  
+        self._rollback_outdated_transactions()
+
     def _transfer_settlements_to_schema(self):
         '''
         Communicates with the SchemaAPI and processes settlement transfers.
         Raises ValueError if smth bad happened.
         '''
-        inner_settlement_account = UserAccountsUnion.objects.get_inner_settlement_account()
-        external_settlement_account = UserAccountsUnion.objects.get_external_settlement_account()
+        inner_settlement_account = UserAccountsUnion.objects.\
+            get_inner_settlement_account()
+        external_settlement_account = UserAccountsUnion.objects.\
+            get_external_settlement_account()
         try:
             SchemaAPI().transfer_debts_to_schema(
-                 amount=inner_settlement_account.base_amount)
+                amount=inner_settlement_account.base_amount)
             self._log_settlement_transaction(
-                 inner_settlement_account=inner_settlement_account,
-                 external_settlement_account=external_settlement_account)
+                inner_settlement_account=inner_settlement_account,
+                external_settlement_account=external_settlement_account)
         except SchemaAPI.SchemaError as e:
             self._alarm_schema_error(e.info)
 
@@ -40,7 +42,8 @@ class Command(BaseCommand):
         '''
         Saves info of successfull settlements as our transfers
         '''
-        # so error-prone logic. should be rewrighten after requirements are specified
+        # so error-prone logic. should be rewrighten after requirements are
+        # specified
         transaction = Transaction.objects.settle_day_transactions(
             kwargs.get('inner_settlement_account').base_amount,
             kwargs.get('inner_settlement_account').base_account,
@@ -49,27 +52,28 @@ class Command(BaseCommand):
     def _alarm_schema_error(self, err_info):
         '''
         Sends schema error to all monitoring channels.
-        ''' 
+        '''
         TelegramAPI().alarm_schema_err(err_info)
         SmsAPI().alarm_schema_err(err_info)
         SendgridAPI().alarm_schema_err(err_info)
-        
+
     def _rollback_outdated_transactions(self):
         '''
         Rollbacks all old authorization transactions 
         without prsentment transactions
         '''
-        transactions_date_treshold = self._get_transactions_date_treshold()
-        outdated_transaction_codes = Transaction.objects.get_non_presented_transactions_before(
-                                                         transactions_date_treshold) 
+        transactions_date_treshold = \
+            self._get_transactions_date_treshold()
+        outdated_transaction_codes = Transaction.objects.\
+            get_non_presented_transactions_before(transactions_date_treshold)
         for code in outdated_transaction_codes:
             Transaction.objects.rollback_late_presentment(code)
 
     def _get_transactions_date_treshold(self):
         '''
         Get date all transactions before which are outdated
-        ''' 
-        return to_start_day(datetime.datetime.now() - \
-                            datetime.timedelta(days=AUTHORISATION_TRANSACTION_TTL))       
-       
-            
+        '''
+        return to_start_day(
+            datetime.datetime.now() - \
+            datetime.timedelta(days=AUTHORISATION_TRANSACTION_TTL))
+        
